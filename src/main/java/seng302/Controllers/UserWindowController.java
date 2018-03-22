354b04ff53e97b2d8097c6b9b6271591b6daebeb
Andrew French <afr66@uclive.ac.nz>
2018-03-22 14:24:14 +1300

    Created the logout function for when a user wants to logout.

--- src/main/java/seng302/Controllers/UserWindowController.java
+++ src/main/java/seng302/Controllers/UserWindowController.java
@@ -1015,6 +1015,27 @@ public class UserWindowController implements Initializable {
         }
     }
 
+//    /**
+//     * Function which is called when the user wants to logout of the application and log into a new user
+//     */
+//    public void logout() {
+//        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+//        alert.setTitle("Are you sure?");
+//        alert.setHeaderText("Are you sure would like to logout? ");
+//        alert.setContentText("Logging out without saving loses your non-saved data.");
+//
+//        Optional<ButtonType> result = alert.showAndWait();
+//        if (result.get() == ButtonType.OK) {
+//            System.out.println("Exiting GUI");
+//            String text = History.prepareFileStringGUI(currentDonor.getId(), "quit");
+//            History.printToFile(streamOut, text);
+//            Main.setCurrentDonor(null);
+//            Main.setScene(TFScene.login);
+//        } else {
+//            alert.close();
+//        }
+//    }
+
     /**
      * Function which is called when the user wants to exit the application.
      */
