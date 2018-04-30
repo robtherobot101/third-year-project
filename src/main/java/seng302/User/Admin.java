0af2a52428285f8f30739ff5ad17660ce4a1995c
Matthew <mkn29@uclive.ac.nz>
2018-04-30 12:57:13 +1200

    Created Admin class
    -Basic constructors, with a deep copy
    -Getters/setters
    -toString method
    
    #story[671]

---
+++ src/main/java/seng302/User/Admin.java
@@ -0,0 +1,53 @@
+package seng302.User;
+
+import seng302.Generic.Main;
+
+/**
+ * This class contains information about clinicians.
+ */
+public class Admin {
+    private String name, username, password;
+    private long staffID;
+
+    public Admin(String username, String password, String name){
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.staffID = Main.getNextId(true, false);
+    }
+
+    /**
+     * Constructor used when making a deep copy of a clinician.
+     * @param originalAdmin the original clinician object being copied.
+     */
+    public Admin(Admin originalAdmin){
+        this.username = originalAdmin.username;
+        this.password = originalAdmin.password;
+        this.name = originalAdmin.name;
+        this.staffID = originalAdmin.staffID;
+    }
+
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
+
+    public long getStaffID() { return staffID; }
+
+    public String getUsername() { return username; }
+
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword() { return password; }
+
+    public void setPassword(String password) { this.password = password; }
+
+    @Override
+    public String toString() {
+        return "Admin{" +
+                "name='" + name + '\'' +
+                ", staffID='" + staffID + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
