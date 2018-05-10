d5a4220e826b0ed8b427d838827e28955a14ba4c
Andrew Davidson <ada130@uclive.ac.nz>
2018-05-10 16:28:03 +1200

    TDD: set up initial test for GUI input into command line
    #pair[ada130, mkn29] #story[713]
    -Added a TDD test to check that input from the GUI CLI text field is
    run through the CLI class.

--- src/test/java/seng302/TestFX/AdminCLITest.java
+++ src/test/java/seng302/TestFX/AdminCLITest.java
@@ -1,14 +1,17 @@
 package seng302.TestFX;
 
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.GridPane;
+import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
+import seng302.Generic.DataManager;
 
 import java.util.concurrent.TimeoutException;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class AdminCLITest extends TestFXTest {
 
@@ -17,21 +20,33 @@ public class AdminCLITest extends TestFXTest {
         defaultTestSetup();
     }
 
-    @Test
-    public void tabExists() {
+    @Before
+    public void setupTest() {
         loginAsDefaultAdmin();
         clickOn("#cliTabButton");
         sleep(200);
+    }
+
+    @Test
+    public void tabExists() {
         GridPane mainPane = lookup("#mainPane").query();
         assertTrue(!mainPane.isVisible()); //Ensure that the search tab is no longer open
     }
 
     @Test
     public void embeddedCliExists() {
-        loginAsDefaultAdmin();
-        clickOn("#cliTabButton");
-        sleep(200);
         TextField textField = lookup("#commandInputField").query();
         assertNotNull(textField); //Ensure that the CLI is now showing
+    }
+
+    @Ignore
+    @Test
+    public void cliInputIsRead() {
+        clickOn("#commandInputField");
+        DataManager.users.clear();
+        write("adduser \"Test,User\" 01/10/1998");
+        press(KeyCode.ENTER);
+        release(KeyCode.ENTER);
+        assertEquals(1, DataManager.users.size());
     }
 }
