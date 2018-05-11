3d04bcef1a4054c1104d4c4308754f3afae0cee1
Kyran Stagg <kst84@uclive.co.nz>
2018-05-11 12:29:05 +1200

    Updated mysql connector version ad made an example class to test connections. also moved database information to their own variables to change them easily

--- src/main/java/seng302/Generic/Database.java
+++ src/main/java/seng302/Generic/Database.java
@@ -1,31 +1,27 @@
 package seng302.Generic;
 
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
 import java.sql.*;
 
 public class Database {
 
     private static String testDatabase = "seng302-2018-team300-test";
+    private static String username = "seng302-team300";
+    private static String password = "WeldonAside5766";
+    private static String url = "jdbc:mysql://mysql2.csse.canterbury.ac.nz/";
+    private static String jdbcDriver = "com.mysql.cj.jdbc.Driver";
 
     public static void main(String args[]) {
         try{
-            System.out.println("Trying to get Driver");
-            Class.forName("com.mysql.jdbc.Driver");
-            System.out.println("Got Driver");
+            Class.forName(jdbcDriver);
             Connection con= DriverManager.getConnection(
-                    "jdbc:mysql://mysql2.csse.canterbury.ac.nz/" + testDatabase,"seng302-team300","WeldonAside5766");
+                    url + testDatabase, username, password);
             System.out.println("Connected to database");
             Statement stmt=con.createStatement();
-            ResultSet rs=stmt.executeQuery("select * from USERS");
-            System.out.println("Got to Users");
+            ResultSet rs=stmt.executeQuery("select * from USER");
             System.out.println("Users:");
 
             while(rs.next())
-                System.out.println(rs.getString(0));
+                System.out.println(rs.getString(1));
             con.close();
         }catch(Exception e){
             System.out.println(e);}
