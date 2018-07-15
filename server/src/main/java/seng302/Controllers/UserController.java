ccaef22d3ad0fc8ce3dd75e77ab49beb04ac4dc6
Andrew Davidson <ada130@uclive.ac.nz>
2018-07-16 01:15:00 +1200

    Added organ donation and waiting list item updates to GET /users/:id
    #story[899]
    -Added a method to update a user's waiting list items from the database
    to a new list of waiting list items. It does this in as few operations
    (database calls) as possible, and edits existing entries where possible
    (instead of removing and then adding an updated version) similarly to
    the other methods used in GET /users/:id
    -Added a method to update a user's organ donations to a new list of
    organ donations by deleting all previously registered organs and then
    adding a new list (because there was no method to remove a single organ
    donation)

--- server/src/main/java/seng302/Controllers/UserController.java
+++ server/src/main/java/seng302/Controllers/UserController.java
@@ -1,19 +1,21 @@
 package seng302.Controllers;
 
+import static java.lang.Integer.max;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonSyntaxException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import seng302.Logic.Database.GeneralUser;
-import seng302.Model.Clinician;
 import seng302.Model.User;
 import seng302.Server;
 import spark.Request;
 import spark.Response;
-
-import java.sql.SQLException;
-import java.util.*;
-
-import static java.lang.Integer.max;
 
 public class UserController {
     private GeneralUser model;
@@ -306,7 +308,8 @@ public class UserController {
             return "Missing User Body";
         } else {
             try {
-                model.updateUserAttributes(receivedUser, Integer.parseInt(request.params(":id")));
+                //model.updateUserAttributes(receivedUser, Integer.parseInt(request.params(":id")));
+                model.patchEntireUser(receivedUser, Integer.parseInt(request.params(":id"))); //this version patches all user information
                 response.status(201);
                 return "USER SUCCESSFULLY UPDATED";
             } catch (SQLException e) {
