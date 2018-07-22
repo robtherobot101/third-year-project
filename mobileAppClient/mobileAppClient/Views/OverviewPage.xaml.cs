1d56822bcdbf2271534cf08283ed5a72b63964b9
Andrew French <afr66@uclive.ac.nz>
2018-07-22 18:31:59 +1200

    Implemented functionality for story 1M: * home_icon.png: * organs_icon.png: * logout_icon.png: * donationIcon.png: * mobileAppClient.iOS.csproj: * overviewImage.jpg: * diseases_icon.png: * attributes_icon.png: * procedures_icon.png: * waitinglist_icon.png: * medications_icon.png: * Resource.designer.cs: * mobileAppClient.Android.csproj: * home_icon.png: * organs_icon.png: * logout_icon.png: * donationIcon.png: * overviewImage.jpg: * diseases_icon.png: * procedures_icon.png: * attributes_icon.png: * medications_icon.png: * waitinglist_icon.png:
    * MainPage.xaml.cs: Added in new icons for the menu slider for both
      iOS and Android platforms
    
    * LoginPage.xaml: Added in a new splash screen icon for the login page
      and title label
    
    * OverviewPage.xaml: Added in an image to land on for the overview
      page and
    new text cells and view cells to display a summary of all user details
    
    * OverviewPage.xaml.cs: Added in a backend for this new updated page
      and populated the cells with appropriate data
    #implement
    
    Conflicts:
    	mobileAppClient/mobileAppClient/MainPage.xaml.cs

--- mobileAppClient/mobileAppClient/Views/OverviewPage.xaml.cs
+++ mobileAppClient/mobileAppClient/Views/OverviewPage.xaml.cs
@@ -14,11 +14,179 @@ namespace mobileAppClient
         {
             InitializeComponent();
             UserController.Instance.addUserObserver(this);
+            if (UserController.Instance.LoggedInUser != null)
+            {
+                fillFields();
+            }
+
         }
 
         public void updateUser()
         {
+            fillFields();
+        }
+
+        public void fillFields() {
             
+            User currentUser = UserController.Instance.LoggedInUser;
+
+
+            //Attributes Pane 
+            //---------------------------------------------------------------------------
+            double age;
+            if (currentUser.dateOfDeath == null)
+            {
+                age = (DateTime.Now - currentUser.dateOfBirth.ToDateTime()).Days / 365.00;
+            }
+            else
+            {
+                age = (currentUser.dateOfDeath.ToDateTime() - currentUser.dateOfBirth.ToDateTime()).Days / 365.00;
+            }
+            string attributesString = "Name: " + String.Join(" ", currentUser.name) + "\nAge: " + String.Format("{0:0.00}", age) + " years old";
+            if (currentUser.currentAddress != null) attributesString += "\nCurrent Address: " + currentUser.currentAddress;
+            if (currentUser.gender.ToString() != "") attributesString += "\nBirth Gender: " + currentUser.gender.ToPickerString();
+            AttributesLabel.Text = attributesString;
+
+            //Organs Pane 
+            //---------------------------------------------------------------------------
+            string organsString = "";
+            if (currentUser.organs.Count == 0)
+            {
+                organsString = "No organs currently donated.";
+            }
+            else
+            {
+                organsString = "Currently donating: \n" + String.Join(", ", currentUser.organs);
+            }
+            OrgansLabel.Text = organsString;
+
+
+            //Medications Pane 
+            //---------------------------------------------------------------------------
+            string currentMedicationsString = "";
+            if (currentUser.currentMedications.Count == 0)
+            {
+                currentMedicationsString = "No medications currently being taken.";
+            }
+            else
+            {
+                List<string> currentMedications = new List<string>();
+                foreach (Medication item in currentUser.currentMedications)
+                {
+                    currentMedications.Add(item.Name);
+                }
+                currentMedicationsString = "Currently taking: \n" + String.Join(", ", currentMedications);
+            }
+
+            string historicMedicationsString = "";
+            if (currentUser.historicMedications.Count == 0)
+            {
+                historicMedicationsString = "No medications have been taken in the past.";
+            }
+            else
+            {
+                List<string> historicMedications = new List<string>();
+                foreach (Medication item in currentUser.historicMedications)
+                {
+                    historicMedications.Add(item.Name);
+                }
+                historicMedicationsString = "Has taken: \n" + String.Join(", ", historicMedications);
+            }
+
+            MedicationsLabel.Text = currentMedicationsString + "\n" + historicMedicationsString;
+
+
+            //Diseases Pane 
+            //---------------------------------------------------------------------------
+            string currentDiseasesString = "";
+            if (currentUser.currentDiseases.Count == 0)
+            {
+                currentDiseasesString = "Not suffering from any diseases currently.";
+            }
+            else
+            {
+                List<string> currentDiseases = new List<string>();
+                foreach (Disease item in currentUser.currentDiseases)
+                {
+                    currentDiseases.Add(item.Name);
+                }
+                currentDiseasesString = "Currently suffering from: \n" + String.Join(", ", currentDiseases);
+            }
+
+            string curedDiseasesString = "";
+            if (currentUser.curedDiseases.Count == 0)
+            {
+                curedDiseasesString = "Has not suffered from any diseases in the past.";
+            }
+            else
+            {
+                List<string> curedDiseases = new List<string>();
+                foreach (Disease item in currentUser.curedDiseases)
+                {
+                    curedDiseases.Add(item.Name);
+                }
+                curedDiseasesString = "Has suffered from: \n" + String.Join(", ", curedDiseases);
+            }
+
+
+            DiseasesLabel.Text = currentDiseasesString + "\n" + curedDiseasesString;
+
+            //Procedures Pane 
+            //---------------------------------------------------------------------------
+
+            string pendingProceduresString = "";
+            if (currentUser.pendingProcedures.Count == 0)
+            {
+                pendingProceduresString = "Not due for any procedures in the future.";
+            }
+            else
+            {
+                List<string> pendingProcedures = new List<string>();
+                foreach (Procedure item in currentUser.pendingProcedures)
+                {
+                    pendingProcedures.Add(item.Summary);
+                }
+                pendingProceduresString = "Procedures due for: \n" + String.Join(", ", pendingProcedures);
+            }
+
+            string previousProceduresString = "";
+            if (currentUser.previousProcedures.Count == 0)
+            {
+                previousProceduresString = "Has not had any procedures in the past.";
+            }
+            else
+            {
+                List<string> previousProcedures = new List<string>();
+                foreach (Procedure item in currentUser.previousProcedures)
+                {
+                    previousProcedures.Add(item.Summary);
+                }
+                previousProceduresString = "Has had the following procedures: \n" + String.Join(", ", previousProcedures);
+            }
+
+            ProceduresLabel.Text = pendingProceduresString + "\n" + previousProceduresString;
+
+            //Waiting List Items Pane 
+            //---------------------------------------------------------------------------
+
+            string waitingListItemsString = "";
+            if (currentUser.waitingListItems.Count == 0)
+            {
+                waitingListItemsString = "Not waiting on any organs to be donated.";
+            }
+            else
+            {
+                List<string> waitingListItems = new List<string>();
+                foreach (WaitingListItem item in currentUser.waitingListItems)
+                {
+                    waitingListItems.Add(item.OrganType);
+                }
+                waitingListItemsString = "Organs waiting on: \n" + String.Join(", ", waitingListItems);
+            }
+
+            WaitingListLabel.Text = waitingListItemsString;
+
+        
         }
     }
 }
