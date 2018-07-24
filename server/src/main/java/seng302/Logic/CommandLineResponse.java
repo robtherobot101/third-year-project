d3c6500074d2ceaaaa338722757c8f6ca5976d20
Kyran Stagg <kst84@uclive.co.nz>
2018-07-24 17:37:51 +1200

    Added history logging for user changes when changes are made through the admin cli and not the GUI. only logs changes to users (excluding deletion) #implement #story[873]

--- server/src/main/java/seng302/Logic/CommandLineResponse.java
+++ server/src/main/java/seng302/Logic/CommandLineResponse.java
@@ -3,10 +3,19 @@ package seng302.Logic;
 public class CommandLineResponse {
     private boolean success;
     private String response;
+    private long userId;
+
     public CommandLineResponse(boolean success, String response){
         this.success = success;
         this.response = response;
     }
+
+    public CommandLineResponse(boolean success, String response, long userId){
+        this.success = success;
+        this.response = response;
+        this.userId = userId;
+    }
+
 
     public boolean isSuccess(){
         return success;
@@ -15,4 +24,6 @@ public class CommandLineResponse {
     public String getResponse(){
         return response;
     }
+
+    public long getUserId() { return  userId; }
 }
