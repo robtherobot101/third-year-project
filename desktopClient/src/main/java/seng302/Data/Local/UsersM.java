b16a0fc967c5e630923caab1c1640526900053ec
Nicky Henricks <nzo13@uclive.ac.nz>
2018-07-24 18:10:53 +1200

    * Fixed up the testFX tests for the clinician window and changed them to only ose local storage
    #story[873]

--- desktopClient/src/main/java/seng302/Data/Local/UsersM.java
+++ desktopClient/src/main/java/seng302/Data/Local/UsersM.java
@@ -58,7 +58,7 @@ public class UsersM implements UsersDAO {
      * @param term The search term which will be broken into space separated tokens
      * @return A sorted list of results
      */
-    private ArrayList<User> getUsersByNameAlternative(String term) {
+    public ArrayList<User> getUsersByNameAlternative(String term) {
         if (term.equals("")) {
             ArrayList<User> sorted = new ArrayList<>(users);
             sorted.sort(Comparator.comparing(User::getName));
@@ -84,8 +84,19 @@ public class UsersM implements UsersDAO {
             if (scoreComparison == 0) {
                 return o1.getName().compareTo(o2.getName());
             }
+
+            System.out.println("{");
+            System.out.println("Name:" + o1.getName() + ", score: " + o1Score);
+            System.out.println("Name:" + o2.getName() + ", score: " + o2Score);
+            System.out.println("Score comparison: " + scoreComparison);
+            System.out.println("}");
+
             return scoreComparison;
         });
+        System.out.println(matched.get(0).getName());
+
+        Collections.reverse(matched);
+        System.out.println(matched.get(0).getName());
         return matched;
     }
 
@@ -105,7 +116,7 @@ public class UsersM implements UsersDAO {
      * @param searchTokens The search tokens which will be compared with the given user's name
      * @return An integer score
      */
-    private int scoreUserOnSearch(User user, List<String> searchTokens) {
+    public int scoreUserOnSearch(User user, List<String> searchTokens) {
         List<String> tokens = new ArrayList<>(searchTokens);
 
         if (!allTokensMatched(user, tokens)) {
@@ -150,7 +161,7 @@ public class UsersM implements UsersDAO {
      * @param weight The weight which will be awarded for each matched name
      * @return An integer score
      */
-    private int scoreNames(String[] names, List<String> tokens, int from, int to, int weight) {
+    public int scoreNames(String[] names, List<String> tokens, int from, int to, int weight) {
         if (names.length >= to && to > from) {
             String[] middleNames = Arrays.copyOfRange(names, from, to);
             int score = 0;
@@ -175,7 +186,7 @@ public class UsersM implements UsersDAO {
      * @param tokens The list of tokens to try against the name
      * @return True if a match was found, otherwise false
      */
-    private boolean nameMatchesOneOf(String name, List<String> tokens) {
+    public boolean nameMatchesOneOf(String name, List<String> tokens) {
         for (String token : tokens) {
             if (lengthMatchedScore(name, token) > 0) {
                 return true;
@@ -193,7 +204,7 @@ public class UsersM implements UsersDAO {
      * @param searchTerm The term which is being looked for
      * @return The length of the longest substring if the searchTerm is matched entirely
      */
-    private int lengthMatchedScore(String string, String searchTerm) {
+    public int lengthMatchedScore(String string, String searchTerm) {
         string = string.toLowerCase();
         searchTerm = searchTerm.toLowerCase();
         int i;
@@ -216,7 +227,7 @@ public class UsersM implements UsersDAO {
      * @param searchTokens The tokens
      * @return True if all tokens match at least one name, otherwise false
      */
-    private boolean allTokensMatched(User user, List<String> searchTokens) {
+    public boolean allTokensMatched(User user, List<String> searchTokens) {
         List<String> tokens = new ArrayList<>(searchTokens);
         for (String name : user.getNameArray()) {
             for (String token : new ArrayList<>(tokens)) {
