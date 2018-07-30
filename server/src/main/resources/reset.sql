2a48028ad7cf27fe3cc2fd8df790a0fbc2f83b18
jmo146 <jmo146@uclive.ac.nz>
2018-07-30 15:52:47 +1200

    removed duplicate insert of token into database from reset sql

--- server/src/main/resources/reset.sql
+++ server/src/main/resources/reset.sql
@@ -160,8 +160,6 @@ CREATE TABLE IF NOT EXISTS `TOKEN` (
   `access_level` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `TOKEN`(`id`, `token`, `access_level`) VALUES (0, 'masterToken',2);
-
 -- --------------------------------------------------------
 
 --
