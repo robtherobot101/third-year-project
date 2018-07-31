8e90a55bca75d2a49281268f98e161cf1810ba82
James MacKay <jma326@uclive.ac.nz>
2018-08-01 02:03:56 +1200

    Created an empty test file for waiting list items

---
+++ server/src/test/java/seng302/Logic/Database/UserWaitingListTest.java
@@ -0,0 +1,36 @@
+package seng302.Logic.Database;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserWaitingListTest extends GenericTest {
+
+    @Test
+    public void getAllWaitingListItems() {
+    }
+
+    @Test
+    public void getAllUserWaitingListItems() {
+    }
+
+    @Test
+    public void getWaitingListItemFromId() {
+    }
+
+    @Test
+    public void insertWaitingListItem() {
+    }
+
+    @Test
+    public void updateWaitingListItem() {
+    }
+
+    @Test
+    public void removeWaitingListItem() {
+    }
+
+    @Test
+    public void removeWaitingListItem1() {
+    }
+}
