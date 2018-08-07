a02f9b608759274ecc73a2a325b5e4c812dc2af1
Matthew Knight <mkn29@uclive.ac.nz>
2018-08-07 22:44:57 +1200

    Completed implementation on user search
    -Changed how dates are calculated
    -Support for searching our given regions
    -CustomDate ToString method
    -Formatting the userlist
    
    #story[1071]

--- mobileAppClient/mobileAppClient/Views/Clinician/UserSearchPage.xaml.cs
+++ mobileAppClient/mobileAppClient/Views/Clinician/UserSearchPage.xaml.cs
@@ -13,7 +13,9 @@ using Xamarin.Forms.Xaml;
 
 namespace mobileAppClient.Views
 {
-
+    /// <summary>
+    /// Clinician only page that contains a Listview of users alongside searching functionality
+    /// </summary>
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class UserSearchPage : ContentPage
     {
@@ -53,6 +55,7 @@ namespace mobileAppClient.Views
             UserListView.ItemsSource = UserList;
             UserListView.RefreshCommand = RefreshCommand;
             UserSearchBar.SearchCommand = SearchCommand;
+            UserSearchBar.TextChanged += UserSearchBar_TextChanged;
 
             UserListView.ItemAppearing += (sender, e) =>
             {
@@ -67,6 +70,25 @@ namespace mobileAppClient.Views
             };
 
             LoadItems();
+        }
+
+        /// <summary>
+        /// Method that is called EVERY time the text within the UserSearchBox is changed
+        /// - Updates the current search query
+        /// - Calls a reset when the search is cleared
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UserSearchBar_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // Update the current search param
+            searchQuery = InputValidation.Trim(e.NewTextValue);
+
+            // Has input been cleared?
+            if (string.IsNullOrEmpty(e.NewTextValue))
+            {
+                ResetItemsQuiet();
+            }
         }
 
         /*
@@ -144,7 +166,7 @@ namespace mobileAppClient.Views
             {
                 return new Command(async () =>
                 {
-                    searchQuery = InputValidation.Trim(UserSearchBar.Text);
+                    ResetItemsQuiet();
                 });
             }
         }
