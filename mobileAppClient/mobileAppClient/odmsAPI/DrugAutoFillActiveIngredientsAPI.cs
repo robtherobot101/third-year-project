d915efb95f607077d0296fb82642a90f477e49de
Andrew French <afr66@uclive.ac.nz>
2018-08-10 14:41:34 +1200

    Implemented functionality for story 8Md: * mobileAppClient.iOS.csproj: * MedicationsPage.xaml: * UserSearchPage.xaml.cs:
    * add_icon.png: Added a new icon for adding a single medication
    
    * User.cs: Fixed a bug where a null pointer was being thrown due to no
      middle name of a user
    
    * DrugAutoFillActiveIngredientsAPI.cs: Created a new API class for
      both the autofill and active ingredients of a medication and to
      handle these calls
    
    * MedicationResponseObject.cs: Created a new response object to handle
      a medications coming back from the api
    
    * MedicationsPage.xaml.cs: Created function to display the new 'add'
      button for creating a new medication
    
    * SingleMedicationPage.xaml: Created a new stack layout to display the
      creation view of a new medication
    
    * SingleMedicationPage.xaml.cs: Created the backend for creating a new
      medication
    #implement #fix

---
+++ mobileAppClient/mobileAppClient/odmsAPI/DrugAutoFillActiveIngredientsAPI.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace mobileAppClient.odmsAPI
+{
+    public class DrugAutoFillActiveIngredientsAPI
+    {
+        public DrugAutoFillActiveIngredientsAPI()
+        {
+        }
+
+        public async Task<List<String>> apiRequest(string url) 
+        {
+            if (!await ServerConfig.Instance.IsConnectedToInternet())
+            {
+                Console.WriteLine("Not connected");
+            }
+
+            // Fetch the url and client from the server config class
+            HttpClient client = ServerConfig.Instance.client;
+
+            HttpResponseMessage response = null;
+            var request = new HttpRequestMessage(new HttpMethod("GET"), url);
+            request.Headers.Add("X-Mashape-Key", "yqCc8Xzox7mshwvnVGeVGRhqb5q7p1QFwldjsnkT3j48eJ4Zfj");
+            request.Headers.Add("Accept", "application/json");
+
+            try
+            {
+                response = await client.SendAsync(request);
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+
+            if (response.StatusCode != HttpStatusCode.OK)
+            {
+                Console.WriteLine("Status code was: " + response.StatusCode);
+            }
+
+            string responseContent = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(responseContent);
+            return new List<string>();
+
+        }
+
+
+        public async Task<List<string>> autocomplete(string query)
+        {
+            query = query.Replace(" ", "+");
+            query = query.Replace("%", "%25");
+            List<string> medications = await apiRequest("https://iterar-mapi-us.p.mashape.com/api/autocomplete?query=" + query);
+            return medications;
+        }
+
+        ///**
+        // * Gets all the active ingredients of a given medicine.
+        // *
+        // * @param medicine The medicine to get the active ingredients of.
+        // * @return Returns the active ingredients as a string arraylist
+        // */
+        //public static String activeIngredients(String medicine)
+        //{
+        //    medicine = medicine.replace(" ", "+");
+        //    medicine = medicine.replace("%", "%25");
+        //    return apiRequest(String.format("https://iterar-mapi-us.p.mashape.com/api/%s/substances.json", medicine));
+        //}
+
+        ///**
+        // * Sends the api requests to MAPI.
+        // *
+        // * @param url The api url to call.
+        // * @return returns a String of the result of the api request.
+        // */
+        //private static String apiRequest(String url)
+        //{
+        //    try
+        //    {
+        //        HttpResponse<JsonNode> response = Unirest.get(url)
+        //                .header("X-Mashape-Key", "yqCc8Xzox7mshwvnVGeVGRhqb5q7p1QFwldjsnkT3j48eJ4Zfj")
+        //                .header("Accept", "application/json")
+        //                .asJson();
+        //        int n;
+        //        n = response.getRawBody().available();
+        //        byte[] bytes = new byte[n];
+        //        response.getRawBody().read(bytes, 0, n);
+        //        return new String(bytes, StandardCharsets.UTF_8);
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        return "";
+        //    }
+        //}
+    }
+}
