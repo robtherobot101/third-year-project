93c0d4c8f69c9edbbcc4f7210f41bf28ade95237
Andrew French <afr66@uclive.ac.nz>
2018-08-11 12:22:53 +1200

    Implemented functionality for story 8Md: * DrugAutoFillActiveIngredientsAPI.cs: Implemented the new active   ingredients API call to retrieve all of the active ingredients of a   single medication
    * MedicationResponseObject.cs: Filed out the medication response
      object class with all of the relevant fields returned from the
      medication apis
    
    * MedicationsPage.xaml.cs: Added in a new refresh method to refresh
      the items in the medications list
    
    * SingleMedicationPage.xaml: Changed the add medication page to be a
      list instead of a table view
    
    * SingleMedicationPage.xaml.cs: Added in a new loading indicator as
      well as the new function to save the medication to the server and
      update the active ingredients as well
    #implement

--- mobileAppClient/mobileAppClient/Views/User/SingleMedicationPage.xaml.cs
+++ mobileAppClient/mobileAppClient/Views/User/SingleMedicationPage.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using mobileAppClient.odmsAPI;
+using mobileAppClient.Models.CustomObjects;
 
 using Xamarin.Forms;
+using System.Net;
 
 namespace mobileAppClient
 {
@@ -13,19 +15,45 @@ namespace mobileAppClient
     public partial class SingleMedicationPage : ContentPage
     {
         DrugAutoFillActiveIngredientsAPI drugAutoFillActiveIngredientsAPI;
+        private bool _IsLoading;
+        private CustomObservableCollection<string> observableMedicationList;
+        public MedicationsPage parentMedicationsPage;
+
+
+        public bool IsLoading
+        {
+            get { return _IsLoading; }
+            set
+            {
+                _IsLoading = value;
+                if (_IsLoading == true)
+                {
+                    LoadingIndicator.IsVisible = true;
+                    LoadingIndicator.IsRunning = true;
+                }
+                else
+                {
+                    LoadingIndicator.IsVisible = false;
+                    LoadingIndicator.IsRunning = false;
+                }
+            }
+        }
 
 
         /*
          * Constructor used to create a new medication
          */ 
-        public SingleMedicationPage()
+        public SingleMedicationPage(MedicationsPage medicationsPage)
         {
-            Console.WriteLine("Gday");
             InitializeComponent();
             AddMedicationLayout.IsVisible = true;
             UserViewLayout.IsVisible = false;
             this.Title = "Add New Medication";
             drugAutoFillActiveIngredientsAPI = new DrugAutoFillActiveIngredientsAPI();
+            observableMedicationList = new CustomObservableCollection<string>();
+            MedicationsList.ItemsSource = observableMedicationList;
+            this.parentMedicationsPage = medicationsPage;
+
 
         }
 
@@ -81,9 +109,79 @@ namespace mobileAppClient
         async void Handle_SearchButtonPressed(object sender, System.EventArgs e)
         {
             // Update the current search param
+            IsLoading = true;
+            observableMedicationList.Clear();
             string searchQuery = InputValidation.Trim(MedicationSearchBar.Text);
-            List<string> medications = await drugAutoFillActiveIngredientsAPI.autocomplete(searchQuery);
-            Console.WriteLine("Hello");
+            MedicationResponseObject medicationsReturned = await drugAutoFillActiveIngredientsAPI.autocomplete(searchQuery);
+            observableMedicationList.AddRange(medicationsReturned.suggestions);
+
+            IsLoading = false;
         }
+
+        async void Handle_MedicationTapped(object sender, Xamarin.Forms.ItemTappedEventArgs e) {
+            if (e == null)
+            {
+                return; //ItemSelected is called on deselection, which results in SelectedItem being set to null
+            }
+            bool answer = await DisplayAlert("Are you sure?", "Do you want to assign " + MedicationsList.SelectedItem + " to " + UserController.Instance.LoggedInUser.FullName + "?", "Yes", "No");
+            if(answer == true) {
+                //Do active ingredients call
+                MedicationResponseObject medicationsReturned = await drugAutoFillActiveIngredientsAPI.activeIngredients(MedicationsList.SelectedItem.ToString());
+
+                //Create new medication object
+
+                Medication newMedication = new Medication();
+                newMedication.Name = MedicationsList.SelectedItem.ToString();
+                newMedication.ActiveIngredients = medicationsReturned.activeIngredients;
+                newMedication.History = new List<string>();
+                newMedication.History.Add("Started taking on " + DateTime.Now.ToString());
+                newMedication.DetailString = newMedication.History[0];
+
+                //Add it to the user's current medications
+
+                UserController.Instance.LoggedInUser.currentMedications.Add(newMedication);
+                User currentUser = UserController.Instance.LoggedInUser;
+                parentMedicationsPage.refreshMedicationsListView();
+
+                await Navigation.PopAsync();
+                //Save user object
+                //UserAPI userAPI = new UserAPI();
+                //HttpStatusCode userUpdated = await userAPI.UpdateUser(true);
+
+                //switch (userUpdated)
+                //{
+                //    case HttpStatusCode.Created:
+                //        await DisplayAlert("",
+                //        "User medications successfully updated",
+                //        "OK");
+                //        await Navigation.PopAsync();
+                //        break;
+                //    case HttpStatusCode.BadRequest:
+                //        await DisplayAlert("",
+                //        "User medications update failed (400)",
+                //        "OK");
+                //        break;
+                //    case HttpStatusCode.ServiceUnavailable:
+                //        await DisplayAlert("",
+                //        "Server unavailable, check connection",
+                //        "OK");
+                //        break;
+                //    case HttpStatusCode.Unauthorized:
+                //        await DisplayAlert("",
+                //        "Unauthorised to modify profile",
+                //        "OK");
+                //        break;
+                //    case HttpStatusCode.InternalServerError:
+                //        await DisplayAlert("",
+                //        "Server error, please try again (500)",
+                //        "OK");
+                //        break;
+                //}
+
+
+            }
+        }
+
+
     }
 }
