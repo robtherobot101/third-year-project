e3be7c837af2e3960da70a583f44ed7360cea3f9
James MacKay <jma326@uclive.ac.nz>
2018-09-05 15:47:52 +1200

    Changed reset endpoint so it drops all tables. This solves the issue where different branches expect different tables in the datbase and the RESET endpoint fails #fix

--- server/src/main/java/seng302/Logic/Database/Administration.java
+++ server/src/main/java/seng302/Logic/Database/Administration.java
@@ -5,6 +5,7 @@ import seng302.Config.SqlFileParser;
 import seng302.Controllers.CLIController;
 import seng302.Model.User;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.sql.*;
@@ -40,11 +41,37 @@ public class Administration {
     /**
      * method to empty the database tables with no values
      * @throws SQLException when the connection to te database has an error
+     * @throws IOException when the resource name has an error
      */
     public void reset() throws SQLException, IOException {
 
         try(Connection connection = DatabaseConfiguration.getInstance().getConnection()) {
+            dropAllTables();
             SqlFileParser.parse(connection, getClass().getResourceAsStream("/reset.sql")).executeBatch();
+        }
+    }
+
+    /**
+     * Drops all tables in the database
+     * @throws SQLException When something goes wrong
+     * @throws IOException This should never occur
+     */
+    private void dropAllTables() throws SQLException, IOException {
+        try (Connection connection = DatabaseConfiguration.getInstance().getConnection()) {
+            // Get statements to delete each table
+            PreparedStatement statement = connection.prepareStatement("SELECT concat('DROP TABLE IF EXISTS ', table_name, ';') " +
+                    "FROM information_schema.tables " +
+                    "WHERE table_schema = ?;");
+            statement.setString(1, DatabaseConfiguration.getInstance().getDatabaseName());
+            ResultSet result = statement.executeQuery();
+            // Combine the statements into a single batch
+            StringBuilder sb = new StringBuilder("SET FOREIGN_KEY_CHECKS = 0; \n");
+            while(result.next()) {
+                sb.append(result.getString(1)).append("\n");
+            }
+            sb.append("SET FOREIGN_KEY_CHECKS = 1;");
+            System.out.println(sb.toString());
+            SqlFileParser.parse(connection, new ByteArrayInputStream(sb.toString().getBytes())).executeBatch();
         }
     }
 }
