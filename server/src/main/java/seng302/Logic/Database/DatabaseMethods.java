904d4afd66dc7ae1527d6887ba796813aba646ff
James MacKay <jma326@uclive.ac.nz>
2018-09-12 23:26:37 +1200

    Closed all resultsets/connections in GeneralUser class #fid

--- server/src/main/java/seng302/Logic/Database/DatabaseMethods.java
+++ server/src/main/java/seng302/Logic/Database/DatabaseMethods.java
@@ -3,6 +3,7 @@ package seng302.Logic.Database;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * Abstract class which all SQL communication classes should extend from
@@ -10,10 +11,12 @@ import java.sql.SQLException;
 public abstract class DatabaseMethods {
 
     protected PreparedStatement statement;
+    protected Statement stmt;
     protected ResultSet resultSet;
 
     void close() throws SQLException {
         try{ if (resultSet != null) resultSet.close(); } catch(SQLException ignored) {}
         try{ if (statement != null) statement.close(); } catch(SQLException ignored) {}
+        try{ if (stmt != null) stmt.close(); } catch(SQLException ignored) {}
     }
 }
