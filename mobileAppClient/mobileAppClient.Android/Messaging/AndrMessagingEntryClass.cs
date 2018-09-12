160a3955345dce8d79988027ae840b9faf297d39
nicky <nicky@DESKTOP-3B2LUK5>
2018-09-13 05:11:36 +1200

    Created a page for messaging on Android. Added model classes for messages and conversations. Added a listener which detects the clicked conversation.
    #Story[1187]

---
+++ mobileAppClient/mobileAppClient.Android/Messaging/AndrMessagingEntryClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(mobileAppClient.Droid.Messaging.AndrMessagingEntryClass))]
+namespace mobileAppClient.Droid.Messaging
+{
+    [Activity(Label = "AndrMessagingEntryClass", MainLauncher = false)]
+    class AndrMessagingEntryClass : Activity, CustomMessagingInterface
+    {
+
+        public void CreateMessagingPage()
+        {
+            Intent intent = new Intent(Forms.Context, typeof(AndrChatViewController));
+            Forms.Context.StartActivity(intent);
+        }
+    }
+}
