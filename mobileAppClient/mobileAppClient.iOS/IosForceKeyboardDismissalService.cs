45fdde064d08fb0b25d9a9fc0379ada2c4b1d86a
Andrew French <frerew101@hotmail.com>
2018-09-25 20:16:21 +1200

    Fixed a couple of bugs with the messaging view - on iOS the keyboard covered the entry so you couldnt see what you were entering #fix

---
+++ mobileAppClient/mobileAppClient.iOS/IosForceKeyboardDismissalService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using mobileAppClient.iOS;
+using mobileAppClient.Views.Messaging;
+using UIKit;
+
+[assembly: Xamarin.Forms.Dependency(typeof(IosForceKeyboardDismissalService))]
+
+namespace mobileAppClient.iOS
+{
+    public class IosForceKeyboardDismissalService : IForceKeyboardDismissalService
+    {
+        public void DismissKeyboard()
+        {
+            UIApplication.SharedApplication.InvokeOnMainThread(() =>
+            {
+                var window = UIApplication.SharedApplication.KeyWindow;
+                var vc = window.RootViewController;
+                while (vc.PresentedViewController != null)
+                {
+                    vc = vc.PresentedViewController;
+                }
+
+                vc.View.EndEditing(true);
+            });
+
+        }
+    }
+}
+
